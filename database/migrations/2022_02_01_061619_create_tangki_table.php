5adf91185a347bba388bdb89459dba328ea67e02
AliffiaRosita <aliffia.rosita@gmail.com>
2022-02-01 14:28:25 +0800

    add migration tangki, sensor, history_sensor

---
+++ database/migrations/2022_02_01_061619_create_tangki_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTangkiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tangki', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->double('tinggi', 15, 8);
+            $table->double('volume', 15, 8);
+            $table->double('suhu', 15, 8);
+            $table->foreignId('id_sensor')->constrained('sensor')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tangki');
+    }
+}
---
+++ database/migrations/2022_02_01_061728_create_sensor_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSensorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensor', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('jenis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensor');
+    }
+}
